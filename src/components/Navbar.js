f12faf136d870742efd2d9a2a5ee76d3faa5269b
TJunkie <tripjunkie24@gmail.com>
2019-12-08 21:29:38 +0300

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <ul className="nav">
-            <li className="nav-item">
-                <a className="nav-link active" href="#">Active</a>
+        <ul>
+            <li>
+                <a href="#">Active</a>
             </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+            <li>
+                <a href="#">Link</a>
             </li>
-            <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+            <li>
+                <a href="#">Link</a>
             </li>
-            <li className="nav-item">
-                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+            <li>
+                <a href="#">Disabled</a>
             </li>
         </ul>
     );
