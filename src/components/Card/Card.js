2609159455f3265baad0d4ddf9a03b726fa18640
tjnk24 <tripjunkie24@gmail.com>
2019-12-14 23:48:40 +0300

    m

---
+++ src/components/Card/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Card.css';
+import ImgProfile from '../../pages/ImgProfile/ImgProfile';
+
+ const Card = (props) => {
+
+    const showImgCard = (event) => {
+        event.preventDefault();
+
+        console.log('ok');
+    };
+
+    return (
+            <a className={classes.Card} href="#" onClick={showImgCard} >
+                <img src={require('../../img/arts_temp_folder/art_1.jpg')} alt="art-card-1"/>
+            </a>
+    )
+}
+
+export default Card;
---
+++ src/pages/ImgProfile/ImgProfile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from '../ImgProfile/ImgProfile';
+import Backdrop from '../../components/Backdrop/BackDrop';
+
+const ImgProfile = (props) => {
+    return (
+        <div className={classes.ImgProfile}>
+            <Backdrop/>
+        </div>
+    )
+}
+
+export default ImgProfile
