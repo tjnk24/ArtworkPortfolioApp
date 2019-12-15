8c6b29d90fd56ce8188e27f10c526b288c59b75d
tjnk24 <tripjunkie24@gmail.com>
2019-12-15 19:36:08 +0300

    m

--- src/pages/ImgProfile/ImgProfile.js
+++ src/pages/ImgProfile/ImgProfile.js
@@ -8,11 +8,11 @@ const ImgProfile = (props) => {
                 <div className={classes.ImgWrap}>
                     <img src={require('../../img/arts_hd_folder/1.jpg')} alt="artwork"/>
                 </div>
-                {/* <div className={classes.VerticalLine}></div>
+                <div className={classes.VerticalLine}></div>
                 <div className={classes.SideBlock}>
                     <h1>GO WEIRD, NOBODY’S WATCHIN’</h1>
                     <p>Это описание для картинки. Я здесь что-нибудь напишу, какое-нибудь крутое описание, чтобы все прочитали и сказали «уай какой харощий картинка хачу хачу хачу себе такой художник».</p>
-                </div> */}
+                </div>
             </div>
         </div>
     )
