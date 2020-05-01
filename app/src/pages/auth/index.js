891fdfd356e20420db1c86058ea0d1fb9a9d56b4
Tjnk <24>
2020-05-01 16:11:26 +0300

    m

--- app/src/pages/auth/index.js
+++ app/src/pages/auth/index.js
@@ -1,35 +1,52 @@
 import React, { useState } from 'react';
+
+import AuthButton from './parts/auth-button';
 import classes from './style';
 
 const Auth = () => {
 
+    const [valid, setValid] = useState(false);
+
     const [loginValue, setLoginValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
 
-    const submitHandler = event => {
-        console.log(loginValue, passwordValue);
+    const validateForms = (login, pass) => {
 
+    }
+
+    const loginHandler = (event) => {
         event.preventDefault();
-    };
+
+        console.log(loginValue, passwordValue);
+    }
 
     return (
         <div className={classes.AuthWrap}>
             <div className={classes.Auth}>
                 <h1>Залогиньтесь, пжлст</h1>
-                <form className={classes.AuthForm} onSubmit={submitHandler} >
+                <form className={classes.AuthForm} onSubmit={loginHandler} >
                     <input
                         type="login"
                         value={loginValue}
                         placeholder="Логин"
-                        onChange={e => {setLoginValue(e.target.value)}}
+                        onChange={e => setLoginValue(e.target.value)}
                     />
                     <input
                         type="password"
                         value={passwordValue}
                         placeholder="Пароль"
-                        onChange={e => {setPasswordValue(e.target.value)}}
+                        onChange={e => setPasswordValue(e.target.value)}
                     />
-                    <button className={classes.Button}>GO!</button>
+                    {valid ?
+                    <p>
+                        Нет такой буквы в этом слове.
+                        <br/>
+                        Ваше очко переходит в зрительный зал.
+                    </p>
+                    : null}
+                    <AuthButton
+                        loginHandler={loginHandler}
+                    />
                 </form>
             </div>
         </div>
