0020a10833ff62650e896e5ea7bb3d57bf99a10f
tjnk24 <tjnk24@gmail.com>
2020-11-25 04:18:49 +0300

    modifying nosleep-job

--- nosleep-job.js
+++ nosleep-job.js
@@ -1,12 +1,10 @@
 const fetch = require('node-fetch');
 
-setInterval(async () => {
-  fetch('https://artwork-portfolio-app.herokuapp.com/ping', {
-    method: 'POST',
-    body: 'no sleep till brooklin',
-  })
-    .then((response) => response.text())
-    .then((result) => {
-      console.log(result);
-    });
-}, 2000);
+fetch('https://artwork-portfolio-app.herokuapp.com/ping', {
+  method: 'POST',
+  body: 'no sleep till brooklin',
+})
+  .then((response) => response.text())
+  .then((result) => {
+    console.log(result);
+  });
